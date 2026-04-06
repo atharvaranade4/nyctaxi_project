f924e36a401c10c3cf2130552dc23477bd16f846
atharvaranade4 <atharvaranade4@gmail.com>
2026-04-06 17:32:38 +0000

    correction: storage acc name

--- transformations/notebooks/04_export/yellow_trips_export.py
+++ transformations/notebooks/04_export/yellow_trips_export.py
@@ -35,7 +35,7 @@ df = df.withColumn("year_month", date_format("tpep_pickup_datetime", "yyyy-MM"))
 # Write the yellow_trips data in JSON format to the External Table "yellow_trips_export"
 
 df.write.\
-    option("path", "abfss://nyctaxi-yellow@nyctaxistorage639.dfs.core.windows.net/yellow_trips_export/").\
+    option("path", "abfss://nyctaxi-yellow@nyctaxistorage01.dfs.core.windows.net/yellow_trips_export/").\
     format("json").\
     mode("append").\
     partitionBy("vendor", "year_month").\
